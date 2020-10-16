f46326bcb36357bf18441ce322d1884354e7bd65
dhofrichter <dhofrichter@samhammer.de>
2020-10-16 09:43:17 +0200

    slightly refactor baseLogger methods to simplify them

--- Samhammer.Logging/BaseLogger.cs
+++ Samhammer.Logging/BaseLogger.cs
@@ -6,48 +6,54 @@
 
     public abstract class BaseLogger : ILog
     {
-        public abstract void Log(string message, LogDetails details, LogLevel loglevel, Exception ex, string memberName, string sourceFilePath, int sourceLineNumber);
+        protected abstract void Log(string message, LogDetails details, LogLevel loglevel, Exception ex);
 
         public void Verbose(string message, LogDetails details = null, Exception ex = null, string memberName = "",
             string sourceFilePath = "", int sourceLineNumber = 0)
         {
-            Log(message, details, LogLevel.Verbose, ex, memberName, sourceFilePath, sourceLineNumber);
+            Log(message, details, LogLevel.Verbose, ex);
         }
 
         public void Debug(string message, LogDetails details = null, Exception ex = null, [CallerMemberName] string memberName = "",
             [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Log(message, details, LogLevel.Debug, ex, memberName, sourceFilePath, sourceLineNumber);
+            Log(message, details, LogLevel.Debug, ex);
         }
 
         public void Info(string message, LogDetails details = null, Exception ex = null, [CallerMemberName] string memberName = "",
             [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Log(message, details, LogLevel.Information, ex, memberName, sourceFilePath, sourceLineNumber);
+            Log(message, details, LogLevel.Information, ex);
         }
 
         public void Warn(string message, LogDetails details = null, Exception ex = null, [CallerMemberName] string memberName = "",
             [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Log(message, details, LogLevel.Warning, ex, memberName, sourceFilePath, sourceLineNumber);
+            Log(message, details, LogLevel.Warning, ex);
         }
 
         public void Error(string message, LogDetails details = null, Exception ex = null, [CallerMemberName] string memberName = "",
             [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Log(message, details, LogLevel.Error, ex, memberName, sourceFilePath, sourceLineNumber);
+            Log(message, details, LogLevel.Error, ex);
         }
 
         public void Fatal(string message, LogDetails details = null, Exception ex = null, string memberName = "",
             string sourceFilePath = "", int sourceLineNumber = 0)
         {
-            Log(message, details, LogLevel.Fatal, ex, memberName, sourceFilePath, sourceLineNumber);
+            Log(message, details, LogLevel.Fatal, ex);
         }
 
         public void Log(string message, LogLevel logLevel, LogDetails details = null, Exception ex = null, string memberName = "",
             string sourceFilePath = "", int sourceLineNumber = 0)
         {
-            Log(message, details, logLevel, ex, memberName, sourceFilePath, sourceLineNumber);
+            details ??= new LogDetails();
+
+            details.Add(LogMetadataFieldNames.MemberName, memberName)
+                .Add(LogMetadataFieldNames.SourceFilePath, sourceFilePath)
+                .Add(LogMetadataFieldNames.SourceLineNumber, sourceLineNumber);
+
+            Log(message, details, logLevel, ex);
         }
 
         public LogDetails Add(string key, object value)
---
+++ Samhammer.Logging/LogMetadataFieldNames.cs
@@ -0,0 +1,12 @@
+﻿namespace Samhammer.Logging
+{
+    /// <summary>
+    /// these fields will be included in the details of all logs. If undesired, they can be removed from the details when implementing BaseLogger.Log()
+    /// </summary>
+    public class LogMetadataFieldNames
+    {
+        public const string MemberName = "MemberName";
+        public const string SourceFilePath = "SourceFilePath";
+        public const string SourceLineNumber = "SourceLineNumber";
+    }
+}

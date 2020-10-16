619622c73d91afa202f09af2db0d1f69bab79470
dhofrichter <dhofrichter@samhammer.de>
2020-10-16 17:07:42 +0200

    Release Samhammer.logging.Serilog

---
+++ Samhammer.Logging.Serilog/SerilogExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Serilog.Events;
+
+namespace Samhammer.Logging.Serilog
+{
+    public static class SerilogExtensions
+    {
+        public static LogEventLevel ToSerilogLogLevel(this LogLevel logLevel)
+        {
+            return logLevel switch
+            {
+                LogLevel.Verbose => LogEventLevel.Verbose,
+                LogLevel.Debug => LogEventLevel.Debug,
+                LogLevel.Information => LogEventLevel.Information,
+                LogLevel.Warning => LogEventLevel.Warning,
+                LogLevel.Error => LogEventLevel.Error,
+                LogLevel.Fatal => LogEventLevel.Fatal,
+                _ => throw new Exception("unknown logLevel")
+            };
+        }
+    }
+}
---
+++ Samhammer.Logging.Serilog/SerilogLogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Serilog.Context;
+
+namespace Samhammer.Logging.Serilog
+{
+    public class SerilogLogger : BaseLogger
+    {
+        protected override void Log(string message, LogDetails details, LogLevel loglevel)
+        {
+            LogWithDetails(() => global::Serilog.Log.Write(loglevel.ToSerilogLogLevel(), message), details);
+        }
+
+        private void LogWithDetails(Action logAction, LogDetails details = null)
+        {
+            var disposables = new List<IDisposable>();
+
+            if (details == null)
+            {
+                logAction();
+                return;
+            }
+
+            foreach (var detailsKey in details.Details.Keys)
+            {
+                var value = details.Details[detailsKey];
+                string serializedValue;
+
+                if (value is string s)
+                {
+                    serializedValue = s;
+                }
+                else
+                {
+                    serializedValue = JsonConvert.SerializeObject(value, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+                }
+
+                disposables.Add(LogContext.PushProperty(detailsKey, serializedValue));
+            }
+
+            logAction();
+            disposables.Reverse();
+            disposables.ForEach(x => x.Dispose());
+        }
+    }
+}

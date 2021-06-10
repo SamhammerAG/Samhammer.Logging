7f67933b470dc5ccacbd739a71f614fdd4595dca
dfuchs <dfuchs@samhammer.de>
2021-06-10 15:06:07 +0200

    Create Logger.cs

---
+++ Samhammer.Logging/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Samhammer.Logging
+{
+    public static class Logger
+    {
+        private static Func<ILog> loggerFactory;
+        
+        public static void Init(Func<ILog> newLogger)
+        {
+            loggerFactory = newLogger;
+        }
+
+        public static ILog Log() => loggerFactory?.Invoke();
+    }
+}

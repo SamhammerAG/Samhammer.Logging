de7bed2bd93d173842f1e9a3988a530588b21249
dfuchs <dfuchs@samhammer.de>
2021-06-15 21:09:23 +0200

    Update BaseLogType.cs

--- Samhammer.Logging/BaseLogType.cs
+++ Samhammer.Logging/BaseLogType.cs
@@ -2,6 +2,8 @@
 
 namespace Samhammer.Logging
 {
+    // This solution based on this idea: https://docs.microsoft.com/de-de/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/enumeration-classes-over-enum-types
+    // To not self implement this, we are using a lib because their solution is going in the same direction.
     public abstract class BaseLogType: SmartEnum<BaseLogType>
     {
         protected BaseLogType(string name, int value) : base(name, value)

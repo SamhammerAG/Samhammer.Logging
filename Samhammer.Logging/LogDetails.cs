673bb4b372ea99295f38358054c4c362e72dfbf0
dhofrichter <dhofrichter@samhammer.de>
2021-08-16 14:40:13 +0200

    AID-20126 remove the warn message when a duplicate key is added

--- Samhammer.Logging/LogDetails.cs
+++ Samhammer.Logging/LogDetails.cs
@@ -10,27 +10,7 @@
 
         public LogDetails Add(string key, object value)
         {
-            if (Details.TryGetValue(key, out _))
-            {
-                if (Details[key] != value)
-                {
-                    Details[key] = value;
-                }
-                else
-                {
-                    var details = new LogDetails();
-                    details.Add("StackTrace", Environment.StackTrace);
-                    details.Add("Key", key);
-                    details.Add("Value", value);
-                    
-                    Logger.Log()?.Warn("Tried to insert duplicate key to log details!", null, details);
-                }
-            }
-            else
-            {
-                Details.Add(key, value);
-            }
-
+            Details[key] = value;
             return this;
         }
 
